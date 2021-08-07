39622a9a298eec72441e31bdf6e2764084976477
hirotsugu.sakurai <sakura09100513@icloud.com>
2021-08-07 05:04:24 +0000

    secound commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,18 @@
 num_max = 100
-(1..num_max).each do |num|
-    def fizzbuzz(num)
-      if num % 3 == 0 && num % 5 == 0
-        puts 'FizzBuzz' 
-      elsif num % 3 == 0
-        puts 'Fizz'
-      elsif num % 5 == 0
-        puts 'Buzz'
-      else 
-        puts num
-      end
-    end
-    puts fizzbuzz(num)
- end
- 
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else 
+    num
+  end
+end
+
+
+(1..num_max).each do |num|   
+  puts fizzbuzz(num)
+end
